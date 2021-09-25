f78b752f0e5d371ae18767038bd6875cc45fd3b3
Dmitriy <example@gmail.com>
2021-09-26 01:11:46 +0600

    Добавил JS (клик на кнопку Заказать / закрыть модальное окно)

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,19 @@ import '../css/style.css';
 /* document.querySelector('#app').innerHTML = `
   <h1>Hello, Dmitriy!</h1>
 `; */
+
+// modal
+
+const modalCloseButton = document.querySelector('[data-close]');
+const modalButton = document.querySelectorAll('[data-modal]');
+const modal = document.querySelector('.overlay');
+
+modalButton.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        modal.classList.toggle('hide');
+    });
+});
+
+modalCloseButton.addEventListener('click', () => {
+    modal.classList.toggle('hide');
+});
